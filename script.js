e23dca3c578f801a30245ee587444cd9ebb9c4b6
Claudia Engelsman <claudia.engelsman@artificialindustry.com>
2017-03-09 16:48:29 +0100

    JQuery for navbar

---
+++ script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Claudia on 9-3-2017.
+ */
+$(document).ready(function () {
+    $(".navigation li").hover(function () {
+        $(this).toggleClass("navigation_on");
+    });
+   /* $(".navigation li").hover (function () {
+        $(this).toggleClass(".navigation li a:hover")
+    })*/
+});
+
+/* Below is TopSecret Game stuff */
+
