83d2fcd67e66b2b8add8137a2ab6b693fd503de5
Claudia Engelsman <claudia.engelsman@artificialindustry.com>
2017-03-09 17:28:24 +0100

    Made balloon image visible and moveable

--- script.js
+++ script.js
@@ -5,10 +5,35 @@ $(document).ready(function () {
     $(".navigation li").hover(function () {
         $(this).toggleClass("navigation_on");
     });
-   /* $(".navigation li").hover (function () {
-        $(this).toggleClass(".navigation li a:hover")
-    })*/
-});
+    /* $(".navigation li").hover (function () {
+     $(this).toggleClass(".navigation li a:hover")
+     })*/
+
 
 /* Below is TopSecret Game stuff */
+
+
+    $("body").keydown(function (key) {
+        switch (parseInt(key.which, 10)) {
+            // Left arrow key pressed
+            case 37:
+                $("#balloon").animate({left: "-=10px"}, 'fast');
+                break;
+            // Up Arrow Pressed
+            case 38:
+                $("#balloon").animate({top: "-=10px"}, 'fast');
+                break;
+            // Right Arrow Pressed
+            case 39:
+                $("#balloon").animate({left: "+=10px"}, 'fast');
+                break;
+            // Down Arrow Pressed
+            case 40:
+                $("#balloon").animate({top: "+=10px"}, 'fast');
+                break;
+        }
+    });
+    
+
+});
 
